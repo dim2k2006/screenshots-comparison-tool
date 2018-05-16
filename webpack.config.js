e84dcdd32f6370e39baa9fd3dfc46a80c8a1504f
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-05-16 23:58:02 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'bundle.js'
+    },
+
+    watch: NODE_ENV === 'development',
+
+    mode: NODE_ENV === 'development' ? 'development' : 'production',
+
+    devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : false,
+
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: `${__dirname}/src`,
+                loader: 'babel-loader',
+                sideEffects: false
+            }
+        ]
+    }
+};
+
+if (NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                sourceMap: false,
+                beautify: false,
+                comments: false,
+                mangle: {
+                    keep_fnames: true
+                },
+                compress: {
+                    sequences: true,
+                    booleans: true,
+                    loops: true,
+                    unused: true,
+                    warnings: false,
+                    drop_console: true
+                }
+            }
+        })
+    );
+}
